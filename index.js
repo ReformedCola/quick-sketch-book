41305d315a8d1053664e2092956edb491dffb06c
ReformedCola <reformedcola@gmail.com>
2020-08-16 10:16:31 -0700

    Feat: clear all

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
-let canvas = document.getElementById("canvas");
-canvas.width = document.documentElement.clientWidth;
-canvas.height = document.documentElement.clientHeight;
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.strokeStyle = 'none';
-ctx.lineWidth = 8;
-ctx.lineCap = "round";
+let canvas = document.getElementById('canvas')
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+let ctx = canvas.getContext('2d')
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'none'
+ctx.lineWidth = 8
+ctx.lineCap = 'round'
 
-let painting = false;
-let last = [];
+let clear = document.getElementById('clear')
+
+
+let painting = false
+let last = []
 
 function drawLine(x1, y1, x2, y2) {
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.stroke()
 }
 
-let isTouchDevice = 'ontouchstart' in document.documentElement;
-console.log(isTouchDevice);
+let isTouchDevice = 'ontouchstart' in document.documentElement
+console.log(isTouchDevice)
 
-if(isTouchDevice)
-{
+if (isTouchDevice) {
   canvas.ontouchstart = (e) => {
-    let x = e.touches[0].clientX;
-    let y = e.touches[0].clientY;
-    last = [x, y];
+    let x = e.touches[0].clientX
+    let y = e.touches[0].clientY
+    last = [x, y]
   }
   canvas.ontouchmove = (e) => {
-    let x = e.touches[0].clientX;
-    let y = e.touches[0].clientY;
-    drawLine(last[0], last[1], x, y);
-    last = [x, y];
+    let x = e.touches[0].clientX
+    let y = e.touches[0].clientY
+    drawLine(last[0], last[1], x, y)
+    last = [x, y]
   }
-}
-else
-{
-  canvas.onmousedown = (e) =>{
-    painting = true;
-    last = [e.clientX, e.clientY];
+} else {
+  canvas.onmousedown = (e) => {
+    painting = true
+    last = [e.clientX, e.clientY]
   }
 
   canvas.onmousemove = (e) => {
-    if(painting === true)
-    {
-      drawLine(last[0], last[1], e.clientX, e.clientY);
-      last = [e.clientX, e.clientY];
+    if (painting === true) {
+      drawLine(last[0], last[1], e.clientX, e.clientY)
+      last = [e.clientX, e.clientY]
     }
   }
 
   canvas.onmouseup = () => {
-    painting = false;
+    painting = false
   }
 }
+
+function clearAll() {
+  clear.onclick = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
+}
+
+clearAll()
