15591b828aadc50ad4e2f06e88491884bbbf7534
ReformedCola <reformedcola@gmail.com>
2020-08-16 14:20:23 -0700

    Organized code

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 let canvas = document.getElementById('canvas')
-canvas.width = document.documentElement.clientWidth
-canvas.height = document.documentElement.clientHeight
 let ctx = canvas.getContext('2d')
-
-
-let lineWidth = 8
 let eraser = document.getElementById('eraser')
 let pencil = document.getElementById('pencil')
 let black = document.getElementById('black')
@@ -17,107 +12,29 @@ let thick = document.getElementById('thick')
 let clear = document.getElementById('clear')
 let save = document.getElementById('save')
 
-let painting = false
-let last = []
-let isEraser = false
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
 
 ctx.fillStyle = 'black'
 ctx.strokeStyle = 'none'
 ctx.lineCap = 'round'
 
-mouseOrTouch(canvas, ctx)
+let lineWidth = 8
+let painting = false
+let isEraser = false
+let last = []
 
-function pencilOrEraser(pencil, eraser) {
-  pencil.onclick = function () {
-    isEraser = false
-    eraser.classList.remove('active')
-    pencil.classList.add('active')
-  }
-  eraser.onclick = function () {
-    isEraser = true
-    pencil.classList.remove('active')
-    eraser.classList.add('active')
-  }
-}
+mouseOrTouch(canvas, ctx)
 
 pencilOrEraser(pencil, eraser)
 
-function colorSwitcher() {
-  black.onclick = function () {
-    ctx.fillStyle = 'black'
-    ctx.strokeStyle = 'black'
-    colors.forEach((color) => {
-      color.classList.remove('active')
-    })
-    black.classList.add('active')
-  }
-  red.onclick = function () {
-    ctx.fillStyle = 'red'
-    ctx.strokeStyle = 'red'
-    colors.forEach((color) => {
-      color.classList.remove('active')
-    })
-    red.classList.add('active')
-  }
-  green.onclick = function () {
-    ctx.fillStyle = 'green'
-    ctx.strokeStyle = 'green'
-    colors.forEach((color) => {
-      color.classList.remove('active')
-    })
-    green.classList.add('active')
-  }
-  blue.onclick = function () {
-    ctx.fillStyle = 'blue'
-    ctx.strokeStyle = 'blue'
-    colors.forEach((color) => {
-      color.classList.remove('active')
-    })
-    blue.classList.add('active')
-  }
-}
+colorSwitcher()
 
-
-function sizeSwitcher() {
-  thin.onclick = function () {
-    lineWidth = 3
-    thick.classList.remove('active')
-    thin.classList.add('active')
-  }
-  thick.onclick = function () {
-    lineWidth = 8
-    thin.classList.remove('active')
-    thick.classList.add('active')
-  }
-}
-
-function clearAll() {
-  clear.onclick = function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-  }
-}
-
-function saveImage() {
-  save.onclick = function () {
-    let url = canvas.toDataURL('image/png')
-    let a = document.createElement('a')
-    document.body.appendChild(a)
-    a.href = url
-    a.download = 'Result'
-    a.target = '_blank'
-    a.click()
-  }
-}
+sizeSwitcher()
 
 clearAll()
 
 saveImage()
-
-
-
-sizeSwitcher()
-
-colorSwitcher()
 
 function mouseOrTouch(canvas, ctx) {
   let isTouchDevice = 'ontouchstart' in document.documentElement
@@ -179,6 +96,86 @@ function touchDraw(canvas, ctx) {
       drawLine(last[0], last[1], x, y)
       last = [x, y]
     }
+  }
+}
+
+function pencilOrEraser(pencil, eraser) {
+  pencil.onclick = function () {
+    isEraser = false
+    eraser.classList.remove('active')
+    pencil.classList.add('active')
+  }
+  eraser.onclick = function () {
+    isEraser = true
+    pencil.classList.remove('active')
+    eraser.classList.add('active')
+  }
+}
+
+
+function colorSwitcher() {
+  black.onclick = function () {
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'black'
+    colors.forEach((color) => {
+      color.classList.remove('active')
+    })
+    black.classList.add('active')
+  }
+  red.onclick = function () {
+    ctx.fillStyle = 'red'
+    ctx.strokeStyle = 'red'
+    colors.forEach((color) => {
+      color.classList.remove('active')
+    })
+    red.classList.add('active')
+  }
+  green.onclick = function () {
+    ctx.fillStyle = 'green'
+    ctx.strokeStyle = 'green'
+    colors.forEach((color) => {
+      color.classList.remove('active')
+    })
+    green.classList.add('active')
+  }
+  blue.onclick = function () {
+    ctx.fillStyle = 'blue'
+    ctx.strokeStyle = 'blue'
+    colors.forEach((color) => {
+      color.classList.remove('active')
+    })
+    blue.classList.add('active')
+  }
+}
+
+function sizeSwitcher() {
+  thin.onclick = function () {
+    lineWidth = 3
+    thick.classList.remove('active')
+    thin.classList.add('active')
+  }
+  thick.onclick = function () {
+    lineWidth = 8
+    thin.classList.remove('active')
+    thick.classList.add('active')
+  }
+}
+
+function clearAll() {
+  clear.onclick = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
+}
+
+function saveImage() {
+  save.onclick = function () {
+    let url = canvas.toDataURL('image/png')
+    let a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = 'Result'
+    a.target = '_blank'
+    a.click()
   }
 }
 
